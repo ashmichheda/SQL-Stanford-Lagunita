85dd57f0997874579f7cb73b5dd4408e53d39355
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-01 00:22:25 +0530

    Social Network query sql exercises

---
+++ SQL-Social-Network-Query-Exercises.sql
@@ -0,0 +1,9 @@
+-- Q1
+-- Find the names of all students who are friends with someone named Gabriel. 
+
+SELECT H1.name
+FROM Highschooler H1 INNER JOIN Friend 
+ON H1.ID = Friend.ID1
+INNER JOIN Highschooler H2
+ON H2.ID = Friend.ID2
+WHERE H2.name = 'Gabriel';
