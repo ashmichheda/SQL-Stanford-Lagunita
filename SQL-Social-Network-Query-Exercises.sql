bc92029f68c7a8fbf9968b6a65dc7ae9625cf81b
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-02 19:12:14 +0530

    Solution for Q3

--- SQL-Social-Network-Query-Exercises.sql
+++ SQL-Social-Network-Query-Exercises.sql
@@ -21,3 +21,13 @@ ON H1.ID = Likes.ID1
 INNER JOIN Highschooler H2
 ON H2.ID = Likes.ID2
 WHERE (H1.grade - H2.grade) >= 2;
+
+-- Q3
+-- For every pair of students who both like each other, return the name and grade of both students. 
+-- Include each pair only once, with the two names in alphabetical order. 
+
+SELECT H1.name, H1.grade, H2.name, H2.grade
+FROM Highschooler H1, Highschooler H2, Likes L1, Likes L2
+WHERE (H1.ID = L1.ID1 AND H2.ID = L1.ID2) 
+AND (H2.ID = L2.ID1 AND H1.ID = L2.ID2) AND H1.name < H2.name
+ORDER BY H1.name, H2.name;
