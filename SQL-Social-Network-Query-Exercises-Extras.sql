0bae44dbaf1d789434d8d998fc79d15d69802a5a
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-02 21:22:51 +0530

    Solution for Q4

--- SQL-Social-Network-Query-Exercises-Extras.sql
+++ SQL-Social-Network-Query-Exercises-Extras.sql
@@ -27,3 +27,23 @@ SELECT AVG(COUNT_PER_STUDENT)
 FROM (	SELECT COUNT(*) AS COUNT_PER_STUDENT
 		FROM Friend 
 		GROUP BY ID1);
+
+
+-- Q4
+-- Find the number of students who are either friends with Cassandra or are friends of friends of Cassandra. 
+-- Do not count Cassandra, even though technically she is a friend of a friend. 
+
+SELECT COUNT(*)
+FROM Friend
+WHERE ID1 IN(SELECT ID2
+			FROM Friend 
+WHERE ID1 in (SELECT ID 
+			  FROM Highschooler
+			  WHERE name = 'Cassandra'));
+	      
+	      
+	      
+	      
+	      
+	      
+	      
