14e0bd78aabba33a35b072325a0cef3762d01722
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-02 20:59:49 +0530

    Solution for Q2

--- SQL-Social-Network-Query-Exercises-Extras.sql
+++ SQL-Social-Network-Query-Exercises-Extras.sql
@@ -5,3 +5,17 @@
 SELECT H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
 FROM Highschooler H1, Highschooler H2, Highschooler H3, Likes l1, Likes L2
 WHERE (H1.ID = L1.ID1 AND H2.ID = L1.ID2) AND (H2.ID = L2.ID1 AND H3.ID = L2.ID2 AND H3.ID <> H1.ID);
+
+
+-- Q2
+-- Find those students for whom all of their friends are in different grades from themselves. 
+-- Return the students' names and grades. 
+
+SELECT H1.name, H1.grade
+FROM Highschooler H1 
+WHERE H1.grade not in (
+						SELECT H2.grade
+						From Highschooler H2, Friend F
+						WHERE H1.ID = F.ID1 AND H2.ID = F.ID2);
+            
+            
