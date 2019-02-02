26ac6ef54611bc9b77a24a0720e40a9058abfd86
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-02 22:10:22 +0530

    solution for Q3

--- SQL-Network-Modification-Exercises.sql
+++ SQL-Network-Modification-Exercises.sql
@@ -16,3 +16,16 @@ AND ID2 NOT IN(SELECT L1.ID1
 	FROM Likes L1 INNER JOIN Friend F
 	USING (ID1)
 	WHERE L1.ID2 = F.ID2);
+	
+	
+-- Q3
+-- For all cases where A is friends with B, and B is friends with C, add a new friendship for the pair A and C. 
+-- Do not add duplicate friendships, friendships that already exist, or friendships with oneself. 
+-- (This one is a bit challenging; congratulations if you get it right.) 
+
+insert into friend
+select f1.id1, f2.id2
+from friend f1 join friend f2 on f1.id2 = f2.id1
+where f1.id1 <> f2.id2
+except
+select * from friend;
