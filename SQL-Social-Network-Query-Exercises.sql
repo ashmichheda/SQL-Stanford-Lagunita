b339c9f2cdcf7ab2cff7697f11f5af878a475244
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-02 19:27:42 +0530

    Solution for Q5

--- SQL-Social-Network-Query-Exercises.sql
+++ SQL-Social-Network-Query-Exercises.sql
@@ -54,3 +54,18 @@ ORDER BY grade, name;
   
   
   
+-- Q5
+-- For every situation where student A likes student B, 
+-- but we have no information about whom B likes (that is, B does not appear as an ID1 in the Likes table), 
+-- return A and B's names and grades. 
+
+SELECT H1.name, H1.grade, H2.name, H2.grade
+FROM Highschooler H1 INNER JOIN Likes
+ON H1.ID = Likes.ID1
+INNER JOIN Highschooler H2
+ON H2.ID = Likes.ID2
+WHERE H2.ID not in (
+	SELECT DISTINCT ID1
+	FROM Likes);
+  
+  
