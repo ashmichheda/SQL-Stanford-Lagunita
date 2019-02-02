c85a4a3cc24f509472c43c8d66e564f253fbd651
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-02 20:21:53 +0530

    Solution for Q7

--- SQL-Social-Network-Query-Exercises.sql
+++ SQL-Social-Network-Query-Exercises.sql
@@ -84,6 +84,39 @@ ORDER BY grade, name;
 
 
 
+-- Q7
+-- For each student A who likes a student B where the two are not friends, 
+-- find if they have a friend C in common (who can introduce them!). 
+-- For all such trios, return the name and grade of A, B, and C. 
+
+SELECT H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
+FROM Highschooler H1, Highschooler H2, Highschooler H3, 
+Friend F1, Friend F2, Likes
+WHERE (H1.ID = Likes.ID1 and H2.ID = Likes.ID2) AND 
+H2.ID NOT IN (
+				SELECT ID2
+				FROM Friend 
+				WHERE ID1 = H1.ID 
+			)
+AND (H1.ID = F1.ID1 AND H3.ID = F1.ID2) AND (H2.ID = F2.ID1 AND H3.ID = F2.ID2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
