4a0d698ec36e4e06925d3a6dbe4483051cbbe13e
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-04 19:24:29 +0530

    added relevant comments

--- Insert_Scripts_Schema.sql
+++ Insert_Scripts_Schema.sql
@@ -1,30 +1,31 @@
+-- SELECT and INSERT scripts.
+-- select scripts for student, college and apply relation
 select * from ashmi_schema.student;
 select * from ashmi_schema.college;
 select * from ashmi_schema.apply;
 
+-- Insert scripts for college relation
 insert into ashmi_schema.college values ('Stanford', 'CA', 15000);
 insert into ashmi_schema.college values ('Berkeley', 'CA', 36000);
 insert into ashmi_schema.college values ('MIT', 'MA', 10000);
 insert into ashmi_schema.college values ('Cornell', 'NY', 21000);
 
+-- Insert scripts for student relation
 insert into ashmi_schema.student values (123, 'Amy', 3.9, 1000);
-
 insert into ashmi_schema.student values (234, 'Bob', 3.6, 1500);
 insert into ashmi_schema.student values (345, 'Craig', 3.5, 500);
 insert into ashmi_schema.student values (456, 'Doris', 3.9, 1000);
 insert into ashmi_schema.student values (567, 'Edward', 2.9, 2000);
 insert into ashmi_schema.student values (678, 'Fay', 3.8, 200);
-
 insert into ashmi_schema.student values (789, 'Gary', 3.4, 800);
 insert into ashmi_schema.student values (987, 'Helen', 3.7, 800);
 insert into ashmi_schema.student values (876, 'Irene', 3.9, 400);
-
 insert into ashmi_schema.student values (765, 'Jay', 2.9, 1500);
 insert into ashmi_schema.student values (654, 'Amy', 3.9, 1000);
 insert into ashmi_schema.student values (543, 'Craig', 3.4, 2000);	
 
+-- Insert scripts for apply relation
 insert into ashmi_schema.apply values (123, 'Stanford', 'CS', 'Y');
-
 insert into ashmi_schema.apply values (123, 'Stanford', 'EE', 'N');
 insert into ashmi_schema.apply values (123, 'Berkeley', 'CS', 'Y');
 insert into ashmi_schema.apply values (123, 'Cornell', 'EE', 'Y');
@@ -43,12 +44,4 @@ insert into ashmi_schema.apply values (765, 'Stanford', 'history', 'Y');
 insert into ashmi_schema.apply values (765, 'Cornell', 'history', 'N');
 insert into ashmi_schema.apply values (765, 'Cornell', 'psychology', 'Y');
 insert into ashmi_schema.apply values (543, 'MIT', 'CS', 'N');
-
-
-
-
-
-
-
-
 
